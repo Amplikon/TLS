d5d43a97c9331e0c7a91434afc03f9d668b380d7
Amplikon <pawciaap2@gmail.com>
2018-03-24 23:18:40 +0100

    First Test Commit

---
+++ TLS/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 24 mar 2018
+ *      Author: Amplikon
+ */
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World 1!" << std::endl;
+	std::cin.get();
+	return 0;
+}
+
+
+
